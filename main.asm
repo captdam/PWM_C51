17705e465eb39e893859ed9b1010f64fa8b10725
Captdam <captdam@beardle.com>
2018-05-15 22:20:23 -0400

    Update main.asm

--- main.asm
+++ main.asm
@@ -19,7 +19,9 @@
 ;P4.4-P4.5 (Pin29-30): Chip ID, config later on board by short (0-3)
 
 ;Input data format (5 bits every time, use 5 bit bus, GPIO):
-;(P0.0L, P0.0H, P0.1L, P0.1H, ..., P0.7L, P0.7H, P1L, P1H, P2L, P2H)*4 CHECK 
+;(P0.0L, P0.0H, P0.1L, P0.1H, ..., P0.7L, P0.7H, P1L, P1H, P2L, P2H)*4, CHECK 
+;P1, P2: ON/OFF signal
+;P0.x: Duty time (0-250)
 ;P3.4	P3.5	P3.6	P3.7
 ;Data.0	Data.1	Data.2	Data.3
 ;4 bits pre word, 20 words pre chip installed, one check word at the end
@@ -40,6 +42,11 @@ ORG 0x03
 
 ;Ini
 INI:
+	;Ini I/O ports
+	MOV P3, #0xFF			;Turn on P3, prepare to read
+	MOV P0, #0x00			;Turn off P0, P1, P2
+	MOV P1, #0x00
+	MOV P2, #0x00
 	;Set chip ID
 	MOV P4, #0xFF			;Prepare to read chip ID
 	MOV A, P4			;Get chip ID
@@ -52,7 +59,7 @@ INI:
 	MUL AB				;times
 	MOV R0, A			;Get Id * 20
 	ADD A, #0x80			;Add 128
-	MOV R4, A			;Get base address, PWM at R4 to R4 + 15, On/OFF at R4 + 16 to R4 + 19
+	MOV R7, A			;Get base address, PWM at R7 to R7 + 15, On/OFF at R7 + 16 to R7 + 19
 	;Enable interupt
 	SETB IT0			;Set INT0 to edge trigger
 	SETB EX0			;Enable INT0
@@ -116,7 +123,7 @@ PROCESS_CHECK:
 
 ;Process P1 and P2: merge 4-bit input data and save result in X-RAM (0xFB and 0xFC)
 PROCESS_P12:
-	MOV A, R4			;Pointer fot input data in RAM (ini: base address)
+	MOV A, R7			;Pointer fot input data in RAM (ini: base address)
 	ADD A, #0x13			;Add 16, which is address of P2High
 	;P2
 	MOV R1, A			;Pointer for input data of P2High in RAM
@@ -141,23 +148,43 @@ PROCESS_P12:
 PROCESS_MERGEPWM:
 	MOV R0, #0x77			;Pointer for merged input data of P0.7
 	PROCESS_READ:
-	DEC R1				;Get XHigh
-	MOV A, @R1
-	MOV R5, A			;Send to R5, prepare for the function
-	DEC R1				;Get XLow
-	MOV A, @R1
-	ACALL FUNC_BITTRANS		;Process data, merge
-	MOV @R0, A			;Save data in RAM
-	DEC R0				;Pointer of merged input data --
-	CJNE R0, #0x6F, PROCESS_READ	;Last one 0x70 (P0), dec then it should be 0x6F
+	  DEC R1			;Get P0.xHigh
+	  MOV A, @R1
+	  MOV R5, A			;Send to R5, prepare for the function
+	  DEC R1			;Get P0.xLow
+	  MOV A, @R1
+	  ACALL FUNC_BITTRANS		;Process data, merge
+	  MOV @R0, A			;Save data in RAM
+	  DEC R0				;Pointer of merged input data --
+	  CJNE R0, #0x6F, PROCESS_READ	;Last one 0x70 (P0), decrease then it should be 0x6F
 	
 ;Process P0, draw map of PWM output, saved in X-RAM slot 0x0001 to 0x00FA, fetch 0x00FA for first step
 PROCESS_MAPPWM:
 	MOV R0, #0xFA			;Pointer for output data in X-RAM (ini: 250)
-	MOV R1, #0xFF			;Ini status of PWM, all high
+	MOV R2, #0xFF			;Ini status of PWM, all high
 	;Loop for 250 times to draw the map
-	PROCESS_PWM:
-	
+	  PWM_OUT:
+	  MOV R1, #0x78			;Set/reset pointer of channel data (last + 1)
+	  CLR A				;Reset ACC
+	  ;Loop for 8 times to process 8 channels
+	    PWM_IN:
+	    RL A			;Get bit of this chennel
+	    DEC R1			;Set pointer to this channe
+	    MOV R3, A			;Save A in R3
+	    MOV A, @R1			;Get time of this channel in ACC
+	    DEC A			;Time of current channel -1
+	    MOV @R1, A			;Save the time (compare with last outer loop, time value = time value - 1)
+	    JNZ	PWM_ON			;If time reachs 0, set to xxxxxxx0, otherwise xxxxxxx1
+	    JMP PWM_DONE
+	    PWM_ON:
+	    INC R3			;Set to xxxxxxx1, without this step, it will be xxxxxxx0
+	    PWM_DONE:
+	    MOV A, R3			;Get A from R3 back
+	    CJNE R2, #0x70, PWM_IN	;All 8 channels process end (for current time), if not, go to PWM_IN
+	  ANL A, R1			;AND with R1 (current PWM), so if any channel reaches 0, turn off that channel
+	  MOV R1, A			;Save current PWM status in R1
+	  MOVX @R0, A			;Save the value in X-RAM (time map)
+	  DJNZ R0, PWM_OUT		;Point to next X-RAM address. If all 250 X-RAM processed, next; otherwise go to PWM_OUT
 	
 ;End of process
 PROCESS_END:
